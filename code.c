f48d1e8993dc4571f1a2813c747e8019eb9e2f81
Matheus Brito <160658617+matheus-sdb@users.noreply.github.com>
2026-03-22 20:30:27 -0300

    Add initial project structure and includes

---
+++ code.c
@@ -0,0 +1,18 @@
+/*
+Projeto 1
+Computação Visual - Ciência da Computação
+Grupo:
+Matheus Santiago de Brito - 10408953
+Fernando Pegoraro Bilia - 10402097
+Carlos Eduardo Rosendo Basseto - 10409941
+Joao Pedro Gianfaldoni - 10409524
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <math.h>
